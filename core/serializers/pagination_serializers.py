cae4b62328749113ac86644ef674284ac719ddfb
cwadven <cwadven@naver.com>
2025-11-26 09:54:57 +0900

    Feat: Add total page

--- core/serializers/pagination_serializers.py
+++ core/serializers/pagination_serializers.py
@@ -5,5 +5,6 @@ class PaginationSerializer(serializers.Serializer):
     current_page = serializers.IntegerField()
     size = serializers.IntegerField()
     total_count = serializers.IntegerField()
+    total_pages = serializers.IntegerField()
     has_next = serializers.BooleanField()
     has_prev = serializers.BooleanField()
